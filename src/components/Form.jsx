7ccb0f86a7b23b63c0c29911d1b89b75bca83058
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-07-22 11:03:57 +0100

    Change the structure of the form: Remove the Input component and use normal inputs inside the form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,49 +1,68 @@
 import { useId } from "react";
-import Input from "./Input";
 
 const Form = () => {
-  const formId = useId();
+  const id = useId();
 
-  const inputs = [
-    {
-      id: "day",
-      name: "day",
-      label: "Day",
-      placeholder: "DD",
-    },
-    {
-      id: "month",
-      name: "month",
-      label: "Month",
-      placeholder: "MM",
-    },
-    {
-      id: "year",
-      name: "year",
-      label: "Year",
-      placeholder: "YYYY",
-    },
-  ];
-
-  const displayInputs = inputs.map((input) => (
-    <Input
-      key={input.id}
-      id={`${formId}-${input.id}`}
-      name={input.name}
-      label={input.label}
-      placeholder={input.placeholder}
-    />
-  ));
   return (
     <form action="" className="form">
-      <div className="fields-wrapper flex gap-7 pb-10">{displayInputs}</div>
-      <div className="border-b w-[560px] relative">
-        <button aria-label="Submit" className="w-16 h-16 bg-primary-purple flex justify-center items-center rounded-full absolute top-1/2 right-0 -translate-y-1/2 transition ease-linear delay-75 hover:bg-neutral-off-black">
-          <img
-            src="/icon-arrow.svg"
-            alt="Submit"
-            className="w-7"
+      <div className="fields-wrapper flex gap-7 pb-10">
+        <div className="input-wrapper">
+          <label
+            htmlFor={`${id}-day`}
+            className="label"
+          >
+            Day
+          </label>
+          <input
+            type="text"
+            id={`${id}-day`}
+            name="day"
+            placeholder="DD"
+            className="input"
           />
+          <small className="error"></small>
+        </div>
+
+        <div className="input-wrapper">
+          <label
+            htmlFor={`${id}-month`}
+            className="label"
+          >
+            Month
+          </label>
+          <input
+            type="text"
+            id={`${id}-month`}
+            name="month"
+            placeholder="MM"
+            className="input"
+          />
+          <small className="error"></small>
+        </div>
+
+        <div className="input-wrapper">
+          <label
+            htmlFor={`${id}-year`}
+            className="label"
+          >
+            Year
+          </label>
+          <input
+            type="text"
+            id={`${id}-year`}
+            name="year"
+            placeholder="YYYY"
+            className="input"
+          />
+          <small className="error"></small>
+        </div>
+      </div>
+      <div className="border-b w-[560px] relative">
+        <button
+          aria-label="Submit"
+          className="w-16 h-16 bg-primary-purple flex justify-center items-center rounded-full absolute top-1/2 right-0 -translate-y-1/2 transition ease-linear delay-75 hover:bg-neutral-off-black"
+        >
+          <img src="/icon-arrow.svg" alt="Submit" className="w-7" />
         </button>
       </div>
     </form>
